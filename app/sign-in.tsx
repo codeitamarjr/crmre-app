e932b5ea69bc35b20fb7cbd943b70df40f9d356a
Itamar Junior <hello@itjunior.dev>
2024-12-31 21:08:45 +0000

    Add global context provider and authentication logic with Appwrite integration

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,13 +1,27 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import images from '../constants/images'
 import icons from '../constants/icons'
+import { login } from '@/lib/appwrite'
+import { useGlobalContext } from '@/lib/global-provide'
+import { Redirect } from 'expo-router'
 
 const SignIn = () => {
+  const { refetch, loading, isLoggedIn } = useGlobalContext();
 
-  const handleLogin = () => { };
+  if (!loading && isLoggedIn) return <Redirect href="/" />
+
+  const handleLogin = async () => {
+    const result = await login();
+
+    if (result) {
+      console.log('Login successful');
+    } else {
+      Alert.alert('Error', 'Failed to login');
+    }
+  };
 
   return (
     <SafeAreaView className='bg-white h-full'>
