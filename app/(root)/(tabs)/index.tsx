9cbf7e1dbe7c3d4358830e697ef146762ac13c4e
Itamar Junior <hello@itjunior.dev>
2025-01-01 17:08:08 +0000

    Add Comment component and implement property fetching by ID; enhance Explore screen with dynamic greeting and loading states

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -62,6 +62,7 @@ export default function Index() {
         ListHeaderComponent={
           <View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
+
               <View className="flex flex-row items-center">
                 <Image source={{ uri: user?.avatar }} className="size-12 rounded-full" />
                 <View className="flex flex-col items-start ml-2 justify-center">
@@ -70,7 +71,8 @@ export default function Index() {
                 </View>
               </View>
 
-              <Image source={icons.bell} className="size-5" />
+              <Image source={icons.bell} className="size-6" />
+
             </View>
 
             <Search />
